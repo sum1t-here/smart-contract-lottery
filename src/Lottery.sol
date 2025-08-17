c053f427945ff356a0a6345ed72fa54e5a729fea
sumit <mazumdarsumit3@gmail.com>
2025-08-18 02:33:45 +0530

    chore: add random number gen logic using vrf

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -1,35 +1,111 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 /**
  * @title A sample Lottery Contract
  * @author Sumit Mazumdar
  * @notice This contract is for creating a sample lottery contract
  * @dev Implements Chainlink VRF
  */
-contract Lottery {
+contract Lottery is VRFConsumerBaseV2Plus {
     /* errors */
     error Lottery__SendMoreEthToParticipate();
+    error Lottery__TrandferFailed();
+    error Lottery__LotteryNotOpened();
 
+    /* type declarations */
+    enum LotteryState {
+        OPEN, // 0
+        CALCULATING // 1
+
+    }
+
+    /* state variables */
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private constant RANDOM_WORDS = 1;
     uint256 private immutable i_entranceFee;
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private i_callbackGasLimit;
     address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+    address private s_recentWinner;
+    LotteryState private s_lotteryState;
 
     /* events */
     event PlayerEntered(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+
+        s_lastTimeStamp = block.timestamp;
+        s_lotteryState = LotteryState.OPEN;
     }
 
-    function enterLottery() public payable {
+    function enterLottery() external payable {
         if (msg.value < i_entranceFee) {
             revert Lottery__SendMoreEthToParticipate();
         }
         s_players.push(payable(msg.sender));
         emit PlayerEntered(msg.sender);
+
+        if (s_lotteryState != LotteryState.OPEN) {
+            revert Lottery__LotteryNotOpened();
+        }
     }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        // check to see if enough time has passed
+        if ((block.timestamp - s_lastTimeStamp) > i_interval) {
+            revert();
+        }
+
+        s_lotteryState = LotteryState.CALCULATING;
+
+        // used from chainlink vrf doc
+        // Will revert if subscription is not set and funded.
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATION,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: RANDOM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal virtual override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        s_lotteryState = LotteryState.OPEN;
+        (bool success,) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Lottery__TrandferFailed();
+        }
+    }
 
     /**
      * getter functions
